95f1fd8ac093c0f2da6a75a5719f3bbe6bc08b79
RavenStephens <bstephenjones90@gmail.com>
2016-11-28 16:28:41 -0800

    Initialized functions
    laid out the basic functions I need

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+var quotes= [];
+var availQuotesIndex=[];
+var printedHTML;
 
+function printQuote(){
+  document.getElementById('quote-box').innerHTML=printedHTML;
+  document.body.style.backgroundColor=getRandomColor();
+
+}
+
+function getRandomQuote(){
+
+}
+
+function getRandomColor(){
+
+}
+
+function getRandomRBG(){
+
+}
+
+
+
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
